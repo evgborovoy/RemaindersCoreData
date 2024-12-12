038eff148378572dbbe1d1048f848248e231d09e
Evgeniy <borovoyevgen@gmail.com>
2024-12-12 17:59:45 +0400

    Initial Commit

---
+++ RemaindersCoreData/RemaindersCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemaindersCoreDataApp.swift
+//  RemaindersCoreData
+//
+//  Created by Evgeniy Borovoy on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RemaindersCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
